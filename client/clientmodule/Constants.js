a235e74bb8d739aaf5549209b5dacbda1c9d5e9f
Adle-watermelon <106425536+Adle-watermelon@users.noreply.github.com>
2025-09-19 13:45:10 +0900

    スマホ版UIの微修正

--- client/clientmodule/Constants.js
+++ client/clientmodule/Constants.js
@@ -1,9 +1,9 @@
 function isMobile() {
   return /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 }
-export const size = isMobile() ? 60*0.9:60;
+export const size = isMobile() ? 60*0.8:60;
 export const charaheight = 31/16;
 export const charawidth = 16/16
 export const synfps = 25;
 export const scale = 2;
-export const fontSize = isMobile() ? 12 * 0.7 : 12;
+export const fontSize = isMobile() ? 12 * 0.6 : 12;
